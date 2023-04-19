8dd582dc536fc3497b1828ba649296079d27fce3
BreeD2 <dormanb@vcu.edu>
2023-04-19 15:33:44 -0400

    a

---
+++ submit-feedback.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $feedback = $_POST['feedback'];
+
+  // TODO: Send email with form data
+  $to = 'you@example.com'; // Replace with your email address
+  $subject = 'Feedback Submission';
+  $message = "Name: $name\nEmail: $email\nFeedback: $feedback";
+  $headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+  mail($to, $subject, $message, $headers);
+
+  // Send success response to client-side
+  http_response_code(200);
+  echo json_encode(['message' => 'Feedback submitted successfully!']);
+} else {
+  // Send error response to client-side
+  http_response_code(400);
+  echo json_encode(['error' => 'Bad Request']);
+}
+?>
